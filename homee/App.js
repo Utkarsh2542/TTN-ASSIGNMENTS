1a2fd687bd64f68f885dd30320412c1d8611972e
Utkarsh Tiwari <utkarsh@Utkarshs-MacBook-Pro.local>
2021-04-19 05:20:47 +0530

    project

--- homee/App.js
+++ homee/App.js
@@ -1,74 +1,127 @@
-import React, {Component} from 'react';
-import {
-  Animated,
-  View,
-  Alert,
-  Image,
-  StyleSheet,
-  PanResponder,
-  TouchableOpacity,
-  Text,
-} from 'react-native';
-class App extends Component {
-  pan = new Animated.ValueXY();
-  panResponder = PanResponder.create({
-    onMoveShouldSetPanResponder: () => true,
-    onPanResponderGrant: () => {
-      this.pan.setOffset({
-        x: this.pan.x._value,
-        y: this.pan.y._value,
-      });
-    },
-    onPanResponderMove: Animated.event([
-      null,
-      {dx: this.pan.x, dy: this.pan.y},
-    ]),
-    onPanResponderRelease: () => {
-      this.pan.flattenOffset();
-    },
-  });
-  Click = () => {
-    Alert.alert('Going to homescreen!');
-  };
+import React, {Component, Fragment} from 'react';
+import {View, Text, StyleSheet, Alert} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import Geolocation, {
+  getCurrentPosition,
+  latitude,
+  longitude,
+} from 'react-native-geolocation-service';
+class Locators extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      coordi: [],
+      markers: [
+        {
+          title: 'Bhopal',
+          coordi: {
+            latitude: 58.4532,
+            longitude: 70.4433,
+          },
+        },
+        {
+          title: 'Sagar',
+          coordi: {
+            latitude: 30.11235,
+            longitude: 89.5436,
+          },
+        },
+        {
+          title: 'Delhi',
+          coordi: {
+            latitude: 45.98,
+            longitude: 84.65,
+          },
+        },
+      ],
+    };
+  }
 
+  handleRegionChange = (mapData) => {
+    this.setState({
+      latitude: mapData.latitude,
+      longitude: mapData.longitude,
+      mapData,
+    });
+  };
+  componentDidMount() {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          coordi: this.state.coordi.concat({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          }),
+        });
+      },
+
+      (error) => {
+        Alert.alert(error.message.toString());
+      },
+      {
+        showLocationDialog: true,
+        enableHighAccuracy: true,
+        timeout: 20000,
+      },
+    );
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.titleText}>Drag this circle!</Text>
-        <Animated.View
-          style={{
-            transform: [{translateX: this.pan.x}, {translateY: this.pan.y}],
-          }}
-          {...this.panResponder.panHandlers}>
-          <View style={styles.box}>
-            <TouchableOpacity onPress={() => this.Click()}>
-              <Image
-                style={styles.iconss}
-                source={require('./draggable.jpg')}
+      <Fragment>
+        <View style={styles.container}>
+          <MapView
+            style={styles.map1}
+            region={{
+              latitude: this.state.latitude,
+              longitude: this.state.longitude,
+              latitudeDelta: 0.8,
+              longitudeDelta: 0.5,
+            }}
+            showsUserLocation={true}
+            onRegionChangeComplete={this.handleRegionChange}>
+            <Marker
+              pinColor={'orange'}
+              title="Current Location"
+              coordinate={{
+                latitude: this.state.latitude,
+                longitude: this.state.longitude,
+              }}
+            />
+            {this.state.markers.map((marker, index) => (
+              <MapView.Marker
+                key={index}
+                coordinate={marker.coordi}
+                title={marker.title}
               />
-            </TouchableOpacity>
-          </View>
-        </Animated.View>
-      </View>
+            ))}
+          </MapView>
+        </View>
+        <View style={styles.text}>
+          <Text>
+            Current Location: {this.state.latitude}, {this.state.longitude}
+          </Text>
+        </View>
+      </Fragment>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    height: 340,
+    width: 400,
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  titleText: {
-    fontSize: 20,
-    lineHeight: 24,
-    fontWeight: 'bold',
+  map1: {
+    ...StyleSheet.absoluteFillObject,
   },
-  iconss: {
-    height: 50,
-    width: 50,
+  text: {
+    marginTop: 400,
   },
 });
 
-export default App;
+export default Locators;
